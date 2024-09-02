80c547dcec59f8808dee1f7e45e6441d7c747358
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-09-02 13:06:26 -0300

    Initial Commit

---
+++ TwitterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TwitterApp
+//
+//  Created by Jamerson Macedo on 02/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TwitterApp/TwitterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAppApp.swift
+//  TwitterApp
+//
+//  Created by Jamerson Macedo on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
