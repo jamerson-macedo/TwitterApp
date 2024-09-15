5c02b72cef5b50f54e1e4405088d78d34df4bc6e
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-09-15 00:31:08 -0300

    Bug Corrigido

--- TwitterApp/Core/MainTab/MainTabView.swift
+++ TwitterApp/Core/MainTab/MainTabView.swift
@@ -11,26 +11,18 @@ struct MainTabView: View {
     @State private var selectedIndex = 0
     var body: some View {
         TabView(selection: $selectedIndex){
-            FeedView().onTapGesture {
-                self.selectedIndex = 0
-            }.tabItem {
+            FeedView().tabItem {
                 Image(systemName: "house")
-            }.tag(1)
-            ExploreView().onTapGesture {
-                self.selectedIndex = 1
-            }.tabItem {
+            }.tag(0)
+            ExploreView().tabItem {
                 Image(systemName: "magnifyingglass")
-            }.tag(2)
-            NotificationsView().onTapGesture {
-                self.selectedIndex = 2
-            }.tabItem {
+            }.tag(1)
+            NotificationsView().tabItem {
                 Image(systemName: "bell")
-            }.tag(3)
-            MessagesView().onTapGesture {
-                self.selectedIndex = 3
-            }.tabItem {
+            }.tag(2)
+            MessagesView().tabItem {
                 Image(systemName: "envelope")
-            }.tag(4)
+            }.tag(3)
         }
     }
 }
